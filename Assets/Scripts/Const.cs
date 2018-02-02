901d3bc56c8ceac280659c69293c42bc2390f72e
Nanxing Xiao <506830915@qq.com>
2018-02-02 22:38:30 +0800

    2018-2-2

---
+++ Assets/Scripts/Const.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Const : MonoBehaviour {
+    public const float  designWidth = 480f;
+    public const float designHeight = 800f;
+
+    public static Matrix4x4 getMatrix()
+    {
+        Matrix4x4 guiMatrix = Matrix4x4.identity;
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+       // Quaternion rotateAngle = (screenWidth > screenHight) ? Quaternion.identity : Quaternion.Euler(90, 0, 0);
+        guiMatrix.SetTRS(new Vector3(0, 0, 0), Quaternion.identity, new Vector3(screenWidth / designWidth, screenHeight / designHeight, 1));
+        return guiMatrix;
+    }
+    public static Matrix4x4 getInvertMatrix()
+    {
+        Matrix4x4 guiInverseMatrix = getMatrix();
+        guiInverseMatrix = Matrix4x4.Inverse(guiInverseMatrix);
+        return guiInverseMatrix;
+    }
+}
